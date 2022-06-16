2a1685a5e451770dfd6819a58b8cd69b7dd6ccc9
Anderson Fariña <inumansoul@gmail.com>
2022-06-16 19:12:10 -0400

    Update CustomerForm.tsx
    Removed unused refs

--- src/pages/contacts/customers/CustomerForm.tsx
+++ src/pages/contacts/customers/CustomerForm.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Button, Card, Container, FormGroup, Input, Label, OutlineButton } from "../../../styles/global";
+import { Button, Card, Container, FormGroup, Input, Label } from "../../../styles/global";
 import Layout from "../../../components/Layout";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
