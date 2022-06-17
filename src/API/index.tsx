b2a7a03d7257d3c89fe8f985e398cda89f0f10d7
Anderson Fariña <inumansoul@gmail.com>
2022-06-17 15:59:08 -0400

    API configurations moved to new files

--- src/API/index.tsx
+++ src/API/index.tsx
@@ -1,5 +1,42 @@
 import axios from "axios";
+import { url, apiHeaders } from "../config";
 
-export const getItems = () => {
-  return 'Get Items function';
+const user = JSON.parse(localStorage.getItem("user") || "{}");
+
+export const doLogin = (form: any) => {
+  axios
+  .post(url.apiUrl + "login", form)
+  .then((res) => {
+    localStorage.setItem("app_token", res.data.success.token);
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+    return 'Logged in!';
+  })
+  .catch((error) => {
+    console.log(error);
+    return error;
+  });
+}
+
+
+export const getItems = async() => {
+  let empresa = {'Empresa': user.empresa_id};
+  apiHeaders.headers = {...apiHeaders.headers, ...empresa}
+
+  const res = await axios.get(url.apiUrl + "items", apiHeaders);
+  return res.data;
+}
+
+export const getSales = async(link:string) => {
+  let empresa = {'Empresa': user.empresa_id};
+  apiHeaders.headers = {...apiHeaders.headers, ...empresa}
+  if (link === '') {
+    const res = await axios.get(url.apiUrl + "sales", apiHeaders);
+    return res.data;
+  }
+  if (link !== '') {
+    const res = await axios.get(link, apiHeaders);
+    return res.data;
+  }
+  const res = await axios.get(link, apiHeaders);
+  return res.data;
 }
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,5 @@
 export const url = {
-  apiUrl: process.env.REACT_API_URL,
+  apiUrl: process.env.REACT_APP_API_URL,
   baseUrl: process.env.REACT_APP_BASE_URL,
 }
 
